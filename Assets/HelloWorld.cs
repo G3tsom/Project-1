ab7147d9ea85614452c163ae78cb9c0152363646
G3tsom <156637745+G3tsom@users.noreply.github.com>
2025-09-25 17:29:18 -0700

    switched from start to update (void)

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -6,8 +6,14 @@ public class HelloWorld : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
+    }
+
+    // Update starts per frame
+    void Update()
+    {
         Debug.Log("Hello World");
     }
 
-    
+
 }
